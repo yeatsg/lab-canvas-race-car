a25bbfb05584482c1b5aaa011abb275fbc861a34
yeatsg <yeatsg@gmail.com>
2022-01-21 11:09:33 -0500

    complete

--- js/index.js
+++ js/index.js
@@ -1,90 +1,163 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-// Basic canvas data //
+  // Basic canvas data //
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 700;
 
-
-
-// Background image code //
+  // Background image code //
   const background = new Image();
-  background.src = '../images/road.png';
+  background.src = "../images/road.png";
   background.onload = () => {
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  }
-
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  };
 
   //car image file//
   const car = new Image();
   car.src = "../images/car.png";
 
-// Player Object //
+  // wall object //
 
-class Player{
-  constructor () {
-    this.w = 40;
-    this.h = 80;
-    this.x = canvas.width/2;
-    this.y = canvas.height - 100;
-    this.image = car
-  }
+  class Item {
+    constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = -40;
+      this.w = 120;
+      this.h = 40;
+      this.color = "brown";
+    }
 
-  move(direction){
-    switch(direction){
-      case "ArrowLeft":
-        if(this.x < 0){
-          this.x = 0;
-        } else {
-          this.x -=10
-        }
-        break;
-      case "ArrowRight":
-        if(this.x > canvas.width-this.w){
-          this.x = canvas.width-this.w;
-        } else {
-          this.x +=10
-        }
-        break;
+    move() {
+      this.y = this.y + 3;
+    }
+
+    pointsEarned() {
+      if (this.y === canvas.height + this.h) {
+        // gameScore += 50;
+        return true;
+      }
     }
   }
-}
 
+  // Player Object //
 
-// declare player character //
+  class Player {
+    constructor() {
+      this.w = 40;
+      this.h = 80;
+      this.x = canvas.width / 2;
+      this.y = canvas.height - 100;
+      this.image = car;
+    }
 
-const driver = new Player ();
-
-
-// movement event listener //
-document.addEventListener("keydown", function (e) {
-  switch (e.code) {
-    case "ArrowLeft":
-      driver.move("ArrowLeft");
-      break;
-    case "ArrowRight":
-      driver.move("ArrowRight");
-      break;
+    move(direction) {
+      switch (direction) {
+        case "ArrowLeft":
+          if (this.x < 0) {
+            this.x = 0;
+          } else {
+            this.x -= 15;
+          }
+          break;
+        case "ArrowRight":
+          if (this.x > canvas.width - this.w) {
+            this.x = canvas.width - this.w;
+          } else {
+            this.x += 15;
+          }
+          break;
+      }
+    }
   }
-});
 
+  // declare player character //
 
+  const driver = new Player();
 
-// Start Game Fnction //
+  const obstcl = new Item();
+
+  let obstclArr = [];
+
+  // movement event listener //
+  document.addEventListener("keydown", function (e) {
+    switch (e.code) {
+      case "ArrowLeft":
+        driver.move("ArrowLeft");
+        break;
+      case "ArrowRight":
+        driver.move("ArrowRight");
+        break;
+    }
+  });
+
+  function createObj() {
+    obstclArr.push(new Item());
+  }
+
+  // Start Game Fnction //
   function startGame() {
+    setInterval(createObj, 4000);
     animate();
   }
 
-function animate () {
-  window.requestAnimationFrame(animate);
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(car, driver.x, driver.y, driver.w, driver.h);  
-}
-}
+  let gameScore = 0;
+  let life = 3;
+  let game;
 
+  // Animate function//
+
+  function animate() {
+    game = window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(car, driver.x, driver.y, driver.w, driver.h);
+    ctx.font = "bold 48px arial";
+    ctx.fillText(`Score = ${gameScore}`, 50, 100);
+
+    // for loop //
+    for (let i = 0; i < obstclArr.length; i++) {
+      ctx.fillStyle = obstclArr[i].color;
+      obstclArr[i].move();
+      ctx.fillRect(
+        obstclArr[i].x,
+        obstclArr[i].y,
+        obstclArr[i].w,
+        obstclArr[i].h
+      );
+      didCollide = detectCollision(driver, obstclArr[i]);
+      if (didCollide) {
+        life--;
+        console.log("COLLISION");
+        obstclArr.splice(i, 1);
+      }
+      pointTaker = obstclArr[i].pointsEarned();
+      if (pointTaker) {
+        obstclArr.splice(i, 1);
+        gameScore += 50;
+      }
+    }
+
+    if (life === 0) {
+      console.log("Game Over!");
+      window.cancelAnimationFrame(game);
+    }
+  }
+
+  function detectCollision(player, obj) {
+    if (
+      player.x < obj.x + obj.w &&
+      player.x + player.w > obj.x &&
+      player.y < obj.y + obj.h &&
+      player.y + player.h > obj.y
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
